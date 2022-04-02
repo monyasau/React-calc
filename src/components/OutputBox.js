66f863fa668314314a351c3a7c661741f589f8e5
Olajide olanrewaju <olajide.m.olanrewaju@gmail.com>
2022-04-02 16:04:12 +0100

    minor update: completed the enter key function

--- src/components/OutputBox.js
+++ src/components/OutputBox.js
@@ -1,8 +1,8 @@
-export default function OutputBox({ firstVal, secVal, lastInput, result }) {
+export default function OutputBox({ firstVal, lastInput, result }) {
   return (
     <div className="border-2 border border-secondary my-3 mx-45 px-2 py-3 w-85 rounded-3" >
 
-    <div className="border-0 rtl py-1 w-100 rounded-3  hidden-scroll">{firstVal + secVal + lastInput}</div>
+    <div className="border-0 rtl py-1 w-100 rounded-3  hidden-scroll">{lastInput + firstVal }</div>
     <input
       type="text"
       value={( result)}
